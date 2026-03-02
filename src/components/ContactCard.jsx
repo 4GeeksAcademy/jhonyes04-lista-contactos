9b876356ddd7a9f310b039c324a4ee5f528bb529
jhonyes04 <jhonyes@hotmail.com>
2026-03-02 09:53:17 +0100

    Proyecto acabado

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -41,16 +41,15 @@ export const ContactCard = ({ contact }) => {
     return (
         <div className="container-fluid">
             <div className="row border border-1 rounded-3 py-2 px-5 align-items-center">
-                <div className="col-12 col-md-2 text-center text-md-start">
+                <div className="col-12 col-md-10 d-flex flex-column flex-sm-row text-center text-sm-start">
                     <img
-                        className="rounded-circle"
+                        className="rounded-circle img-thumbnail mx-auto mx-sm-0"
                         src={`https://api.dicebear.com/9.x/thumbs/svg?seed=${contact.name}`}
                         alt={contact.name}
                         width={100}
                     />
-                </div>
-                <div className="col-12 col-md-8 text-center text-md-start">
-                    <div className="d-flex flex-column text-muted ms-4">
+
+                    <div className="d-flex flex-column text-muted mx-auto ms-sm-5">
                         <p className="m-0 fw-bold fst-italic">{contact.name}</p>
                         <p className="m-0">
                             <i className="fa-solid fa-location-dot me-2"></i>
@@ -66,7 +65,7 @@ export const ContactCard = ({ contact }) => {
                         </p>
                     </div>
                 </div>
-                <div className="col-12 col-md-2 d-flex flex-row justify-content-center mt-2 flex-md-column align-items-md-end mt-md-0 gap-2">
+                <div className="col-12 col-md-2 d-flex flex-row justify-content-center mt-3 flex-md-column align-items-md-end mt-md-0 gap-2">
                     <Link
                         to={`edit-contact/${contact.id}`}
                         className="btn btn-warning"
